f82c598fe8dc60725d31e9e4db2d7b022bf13ad0
rcm2005 <rcm2005@icloud.com>
2025-06-06 19:11:34 -0300

    atualizando readme

--- obj/Debug/net8.0/ServidorTesteSync.AssemblyInfo.cs
+++ obj/Debug/net8.0/ServidorTesteSync.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServidorTesteSync")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0d5d70e9eb3ab1cbba07d11e2d0d04f24b39a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServidorTesteSync")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServidorTesteSync")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
